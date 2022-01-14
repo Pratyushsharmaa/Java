2ae3dbdd4d69cced61dc48aa6d1bd4124f852eaf
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2022-01-15 00:55:57 +0530

    Update leapYearCalculator.java

--- leapYearCalculator.java
+++ leapYearCalculator.java
@@ -1,23 +1,28 @@
 //calculate leap year.
 
 public class leapYearCalculator {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         isLeapYear(2000);
+        isLeapYear(1999);
+        isLeapYear(2321);
+        isLeapYear(-1);
     }
 
     public static boolean isLeapYear(int year) {
-        if (year < 0 || year > 9999) {
-            System.out.println(false);
-            return false;
-        } else if ((year % 4 == 0 && year % 100 == 0 && year % 400 == 0) && (year > 0 && year < 9999)) {
-            System.out.println(true);
-            return true;
+        if (year >= 1 && year <= 9999) {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println("true");
+                return true;
+            } else {
+                System.out.println("false");
+                return false;
+            }
+
         } else {
-            System.out.println(false);
+            System.out.println("false");
             return false;
         }
     }
 }
-
 
 
