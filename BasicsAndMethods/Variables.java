7bd3bd595607d6f57ca5d6e47750f989b68eb198
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2022-01-16 02:02:59 +0530

    Add files via upload

---
+++ BasicsAndMethods/Variables.java
@@ -0,0 +1,14 @@
+//Input three numbers. Add them and print value. Subtract 10 and print final value.
+
+
+public class Variables {
+    public static void main(String[] args) {
+        int firstNum = 5;                 //here 5 is known as a literal value or a luteral
+        int secondNum = 10;
+        int thirdNum = 20;
+        int myTotal = firstNum + secondNum + thirdNum;
+        int myLast = myTotal - 10;
+        System.out.println(myTotal);
+        System.out.println(myLast);
+    }
+}
