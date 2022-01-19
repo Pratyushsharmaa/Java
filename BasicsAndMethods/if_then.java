e9fdbfa1dc3c77fdc693c1a9d8c6b72dc43fbf2d
Pratyush Sharma <37974007+Pratyushsharmaa@users.noreply.github.com>
2022-01-20 05:15:36 +0530

    Add files via upload

---
+++ BasicsAndMethods/if_then.java
@@ -0,0 +1,9 @@
+public class if_then {
+
+    public static void main(String[] args) {
+        boolean isAlien = false;
+        if(isAlien == false) { //remember putting braces
+            System.out.println("No aliens!!!");
+        }
+    }
+}
